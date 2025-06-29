286059ff19a5e63007ca4da21ed112742f828078
Vidhuprasad-iocod <Vidhuprasad@iocod.com>
2025-06-30 00:29:13 +0530

    feat: add best hotels and resorts pages with SEO-optimized metadata and hotel listings

--- pages/hotel/best-hotels-in-hsr-layout.vue
+++ pages/hotel/best-hotels-in-hsr-layout.vue
@@ -3,7 +3,7 @@ import HotelCard from '~/components/hotel/Card.vue'
 import HotelFilters from '~/components/hotel/Filters.vue'
 
 useHead({
-  title: 'Bangalore Today – Discover, Explore & Experience the City',
+  title: 'Best Hotels in HSR Layout',
   link: [
     { rel: 'icon', href: '/images/logo/favicon.png' }
   ],
--- pages/resorts/index.vue
+++ pages/resorts/index.vue
@@ -2,6 +2,37 @@
 import HotelCard from '~/components/hotel/Card.vue'
 import HotelFilters from '~/components/hotel/Filters.vue'
 
+useHead({
+  title: 'Bangalore Today – Discover, Explore & Experience the City',
+  link: [
+    { rel: 'icon', href: '/images/logo/favicon.png' }
+  ],
+
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      name: 'description',
+      content: 'Bangalore Today – Your one-stop destination to find flights, getaways, city life, resorts, restaurants, and much more — all in one simple search.',
+    },
+    {
+      name: 'og:site_name',
+      content: 'Bangalore Today – Your one-stop destination to find flights, getaways, city life, resorts, restaurants, and much more — all in one simple search.',
+    },
+    {
+      name: 'og:url',
+      content: 'https://in.bangaloretoday.in',
+    },
+    {
+      name: 'keywords',
+      content: 'Bangalore, Bangalore Today, Bangalore Flights, Bangalore Getaways, Bangalore City Life, Bangalore Resorts, Bangalore Restaurants, Bangalore Hotels',
+    },
+  ],
+})
+
 const activeSort = ref('Popular')
 
 const hotels = ref([
